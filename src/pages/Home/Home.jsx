03437f9e1daec15a47da6a9bb2ed4770cdefdf77
kadirdolek <kadirdolek@outlook.com>
2025-05-28 11:41:56 +0200

    Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -15,13 +15,28 @@ export default function Home(){
         <>
             <div id='home'>
                 <Navbar/>
-                <p style={{marginTop:'17.5%'}}>
-                    So, you want to travel to Space
-                    Let’s face it; if you want to go to space, you might as well genuinely go to 
-                    outer space and not hover kind of on the edge of it. Well sit back, and relax 
-                    because we’ll give you a truly out of this world experience!
-                </p>
-                <Link to="/destination" id='explore'>Explore</Link>
+                <div className="home-content">
+                    <div className="home-flex">
+                        <div className="home-text">
+                            <p id='titreHome' style={{ marginTop: '17.5%', display:'flex', flexDirection:'column', textAlign:'center' }}>
+                              <div>
+                                    <h2 id='h2home'>
+                                        So, you want to travel to     
+                                    </h2>
+                                    <h1 style={{ fontSize: '90px', marginBottom:'60px', marginTop:'60px' }}>SPACE</h1>
+                              </div>
+                              <div id='textHome' style={{ fontSize: '15px' }}>
+                                    Let’s face it; if you want to go to space, you might as well genuinely go to 
+                                    outer space and not hover kind of on the edge of it. Well sit back, and relax 
+                                    because we’ll give you a truly out of this world experience!
+                              </div>
+                            </p>
+                        </div>
+                        <div id='idexplore'>
+                            <a id="explore" href="/destination">Explorer</a>
+                        </div>
+                    </div>
+                </div>
             
             </div>
         </>
